6954bca5430d569ce2744020b048bd9778c2c7ef
Samith Hegde <samith.hegde@bizacuity.local>
2024-08-01 15:15:25 +0530

    Added basic functionalities

--- script.js
+++ script.js
@@ -43,17 +43,53 @@ function evaluate(operand1, operator, operand2) {
   }
 }
 
-//displayScreen.textContent = '0';
-
+displayScreen.textContent = '0';
 
 document.querySelectorAll('.btn').forEach((button) => {
   button.addEventListener('click', (event) => {
     event.preventDefault();
     if (displayScreen.textContent === '0') {
-      displayScreen.innerHTML = '';
       displayScreen.textContent = button.textContent;
     } else {
       displayScreen.textContent += button.textContent;
     }
   });
 });
+
+const clearButton = document.getElementById('clearButton');
+const backButton = document.getElementById('backButton');
+const equalsButton = document.getElementById('equalsButton');
+
+function handleClearButton() {
+  displayScreen.textContent = '0';
+}
+
+function handleBackButton() {
+  const displayString = displayScreen.textContent;
+  if (displayString.length === 1) {
+    displayScreen.textContent = '0';
+  } else {
+    displayScreen.textContent = displayString.slice(0, displayString.length - 1);
+  }
+}
+
+function handleEqualsButton() {
+  const displayString = displayScreen.textContent;
+  console.log(displayString);
+  const operatorIndex = displayString.search(/[\+\-\*\/\%]/);
+  if (operatorIndex === -1) {
+    displayScreen.textContent = displayString;
+    return;
+  }
+  operand1 = parseFloat(displayString.slice(0, operatorIndex));
+  console.log(displayString.slice(0, operatorIndex));
+  console.log(operand1);
+  operator = displayString[operatorIndex];
+  operand2 = parseFloat(displayString.slice(operatorIndex + 1));
+  console.log(operand2);
+  displayScreen.textContent = evaluate(operand1, operator, operand2);  
+}
+
+backButton.addEventListener('click', handleBackButton);
+clearButton.addEventListener('click', handleClearButton);
+equalsButton.addEventListener('click', handleEqualsButton);
